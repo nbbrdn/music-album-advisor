e38491f2ec29e3a8c5df7cbdf1c1ab4284b67dec
Nikolay Bayborodin <nbbrdn@gmail.com>
2023-10-01 19:41:06 +1000

    Implement the random album api endpoint

--- backend/catalog/admin.py
+++ backend/catalog/admin.py
@@ -7,6 +7,7 @@ from .models import MusicAlbum
 @admin.register(MusicAlbum)
 class MusicAlbumAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "year", "artist")
+    list_display_links = ("title",)
     readonly_fields = ("thumbnail_preview",)
 
     def thumbnail_preview(self, obj):
--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import MusicAlbum
+from .serializers import MusicAlbumSerializer
+
+
+@api_view(["GET"])
+def random_music_album(request):
+    albums = list(MusicAlbum.objects.all())
+    random_album = random.sample(albums, 1)[0]
+    serializer = MusicAlbumSerializer(random_album)
+    return Response(serializer.data)
